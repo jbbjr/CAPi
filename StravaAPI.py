ea0a643a8b7fd426cf4d3a89a96c21de559afc97
Bennett Blanco <109486714+jbblancojr@users.noreply.github.com>
2023-12-29 16:01:02 -0500

    Changing target vector and supplying newly tuned model

--- StravaAPI.py
+++ StravaAPI.py
@@ -1,17 +1,20 @@
 import requests
 import pandas as pd
 import urllib3
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class Client:
-    def __init__(self, client_id, client_secret, refresh_token):
+    def __init__(self, client_id, client_secret, refresh_token, weather_key):
         self.base_url = 'https://www.strava.com/api/v3/'
         self.auth_url = 'https://www.strava.com/oauth/token'
         self.client_id = client_id
         self.client_secret = client_secret
         self.refresh_token = refresh_token
+        self.weather_key = weather_key
         self.access_token = self.get_access_token()
 
     def get_access_token(self):
@@ -58,6 +61,16 @@ class Client:
 
         return ids
 
+    # returns the most recent activity
+    # returns the id of it to process it in get detailed activity
+    def get_recent_activity(self):
+        url = f'{self.base_url}athlete/activities'
+        header = {'Authorization': 'Bearer ' + self.access_token}
+
+        param = {'per_page': 1, 'page': 1}
+        result = requests.get(url, headers=header, params=param).json()
+        return result[0]['id']
+
     # requests detailed activity information from the strava API
     # takes an activity_id (from a respective workout)
     def get_detailed_activity(self, activity_id=int):
@@ -98,6 +111,38 @@ class Client:
             relevant_data['start_long'] = 'NaN'
 
         return relevant_data
+
+    # Integrates the add_weather.py script into the class for a cleaner main
+    def add_weather(self, activity_id=int):
+        base = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline'
+        params = f'?key={self.weather_key}&contentType=csv&unitGroup=us&include=current'
+        activity = self.build_activity_laps_df(activity_id)
+
+        activity_weather = pd.DataFrame()
+        # get live weather data per lap in each activity, add back some unique identifier data so we can join later.
+        for index, lap in activity.iterrows():
+            query = f'{base}/{lap["start_lat"]},{lap["start_long"]}/{lap["start_date_local"].replace("Z", "")}' + params
+            # print(query)
+            temp = pd.read_csv(query)
+            temp['activity_id'] = lap['activity_id']
+            temp['name'] = lap['name']
+            activity_weather = pd.concat([activity_weather, temp])
+
+        return activity_weather
+
+    # For debugging purposes
+    # Prints out the variables as you walk through the method
+    def test_get_recent_activity(self):
+        url = f'{self.base_url}athlete/activities'
+        header = {'Authorization': 'Bearer ' + self.access_token}
+        param = {'per_page': 1, 'page': 1}
+
+        result = requests.get(url, headers=header, params=param).json()
+
+        print(url)
+        print(header)
+        print(param)
+        print(result)
 
     # For debugging purposes
     # Prints out the variables as you walk through the method
